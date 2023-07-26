0b081d6144a817cd9e2cb4448adb8b7591a9a884
Nitesh Kumar <imnksid@gmail.com>
2023-07-26 19:17:12 +0530

    Initial react native code setup

---
+++ App.tsx
@@ -0,0 +1,6 @@
+import { StyleSheet, Text, View } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+export default function App() {
+  return <View />;
+}
