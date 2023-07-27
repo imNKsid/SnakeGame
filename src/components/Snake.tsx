c8978e9e5edb59b7c3d0011de03f6ab1008d0df2
Nitesh Kumar <imnksid@gmail.com>
2023-07-27 13:44:20 +0530

    Added comments for moveSnake function to understand it better & removed unused imports

--- src/components/Snake.tsx
+++ src/components/Snake.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import React, { Fragment } from "react";
 import { Coordinate } from "../types/types";
 import { Colors } from "../styles/colors";
